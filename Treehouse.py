5a005ed74efdf2cdf87644e8c7de904071aa868c
TonZaga <anthony.branham1@gmail.com>
2021-01-11 19:36:34 -0500

    Adding Jupyter Notebook/VSCode test

--- Treehouse.py
+++ Treehouse.py
@@ -268,20 +268,26 @@
 # Practice Comparsions in python #
 #-----------------------------------------------------------------------#
 
-name = input("Please enter your name: ")
-number = input("Please enter a number: ")
-number = int(number)
+# name = input("Please enter your name: ")
+# number = input("Please enter a number: ")
+# number = int(number)
     
-is_fizz = number % 3 == 0
-is_buzz = number % 5 == 0
+# is_fizz = number % 3 == 0
+# is_buzz = number % 5 == 0
 
-print("Hey, {}!\nYour number is {}".format(name, number))
+# print("Hey, {}!\nYour number is {}".format(name, number))
 
-if is_fizz and is_buzz:
-    print("{} is a FizzBuzz number.".format(number))
-elif is_fizz:
-    print("{} is a Fizz number.".format(number))
-elif is_buzz:
-    print("{} is a Buzz number.".format(number))
-else:
-    print("{} is neither a fizzy or a buzzy number".format(number))
+# if is_fizz and is_buzz:
+#     print("{} is a FizzBuzz number.".format(number))
+# elif is_fizz:
+#     print("{} is a Fizz number.".format(number))
+# elif is_buzz:
+#     print("{} is a Buzz number.".format(number))
+# else:
+#     print("{} is neither a fizzy or a buzzy number".format(number))
+
+
+#-----------------------------------------------------------------------#
+# Practice Using Strings and Lists in python #
+#-----------------------------------------------------------------------#
+
