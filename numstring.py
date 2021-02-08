9c96d850e64e9e23b48d6f00c1ed14bd517af25e
TonZaga <anthony.branham1@gmail.com>
2021-02-08 16:54:18 -0500

    Updating Treehouse and note files

--- numstring.py
+++ numstring.py
@@ -10,3 +10,18 @@ class Numstring:
 
     def __float__(self):
         return float(self.value)
+
+    # def __add__(self, other):
+    #     return self.value + str(other)
+
+    def __add__(self, other):
+        if '.' in self.value:
+            return float(self) + other
+        return int(self) + other
+
+    def __radd__(self, other):
+        return self + other
+    
+    def __iadd__(self, other):
+        self.value = self + other
+        return self.value
